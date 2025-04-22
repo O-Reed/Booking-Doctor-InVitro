13e0c8e84c86776d50a3a9abef47347964f5565a
Owen Reed <o.reed.super@gmail.com>
2025-04-22 17:32:15 -0400

    initial React app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold mb-4">Doctor Booking App</h1>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
